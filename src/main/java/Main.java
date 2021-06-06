1be9c1d6f8db48a699ba013ac1908ba227fc5ed0
Sachman19 <sachindmishra@knights.ucf.edu>
2021-06-06 07:46:34 -0400

    S

---
+++ src/main/java/Main.java
@@ -0,0 +1,20 @@
+/*
+ *  UCF COP3330 Summer 2021 Assignment 20 Solution
+ *  Copyright 2021 Sachin Mishra
+ */
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("What is the order amount? ");
+        double order = input.nextDouble();
+
+        input.nextLine(); //Newline gets stuck in the buffer without this.
+
+
+
+    }
+}
