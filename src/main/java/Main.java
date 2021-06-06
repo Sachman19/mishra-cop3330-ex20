a0a44548a29234c83a007213792f3c95d64f6ba0
Sachman19 <sachindmishra@knights.ucf.edu>
2021-06-06 07:55:21 -0400

    V1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,12 +9,35 @@ public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
+        double tax = 0.0;
+
         System.out.print("What is the order amount? ");
         double order = input.nextDouble();
 
         input.nextLine(); //Newline gets stuck in the buffer without this.
 
+        System.out.print("What state do you live in? ");
+        String state = input.nextLine();
 
+        if(state.equalsIgnoreCase("Wisconsin") || state.equalsIgnoreCase("WI")) {
+            tax += 0.05;
+            System.out.print("What county do you live in? ");
+            String county = input.nextLine();
+            if(county.equalsIgnoreCase("Dunn")){
+                tax+= 0.004;
+            }
+            if(county.equalsIgnoreCase("Eau Claire")){
+                tax+= 0.005;
+            }
+        }
+        else if(state.equalsIgnoreCase("Illinois")){
+            tax += 0.08;
+        }
 
+        double total = tax * order; //Temporarily using this to store the tax
+        System.out.printf("The tax is %.2f", total);
+
+        total += order;
+        System.out.printf("The total is %.2f", total);
     }
 }
